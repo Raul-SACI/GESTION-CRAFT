382b3e9074db60d44520ce32303a16591f7678f0
Raul Diaz <raul@gestion-craft.com>
2026-05-29 18:46:13 +0000

    feat: columna Forma de Pago (Transferencia/Efectivo) en Liquidación Mensual

--- src/components/HrMonthlyPayrollView.tsx
+++ src/components/HrMonthlyPayrollView.tsx
@@ -39,7 +39,8 @@ export default function HrMonthlyPayrollView({ branches, selectedMonth, setSelec
     baseRateOrSalary: 3200,
     holidayDaysCount: 0,
     additionalBonus: 0,
-    novedades: ''
+    novedades: '',
+    paymentMethod: 'transferencia'
   });
 
   const payrollBranches = [
@@ -165,6 +166,7 @@ export default function HrMonthlyPayrollView({ branches, selectedMonth, setSelec
           additionalBonus: 0,
           novedades: '',
           totalToCollect: computed,
+          paymentMethod: 'transferencia', // default
         };
       });
 
@@ -196,9 +198,9 @@ export default function HrMonthlyPayrollView({ branches, selectedMonth, setSelec
       return;
     }
     
-    const headers = "Sucursal\tNombre Colaborador\tPuesto\tModalidad\tHoras Reales\tSueldo/Valor Base\tFeriados Trab.\tBono/Adicional\tNovedades\tTotal a Cobrar";
+    const headers = "Sucursal\tNombre Colaborador\tPuesto\tModalidad\tHoras Reales\tSueldo/Valor Base\tFeriados Trab.\tBono/Adicional\tNovedades\tForma de Pago\tTotal a Cobrar";
     const rows = filtered.map(item => {
-      return `${item.branchName}\t${item.employeeName}\t${item.positionLabel}\t${item.salaryType === 'hourly' ? 'POR HORA' : 'MENSUAL'}\t${item.salaryType === 'hourly' ? item.hoursWorked : '-'}\t${item.baseRateOrSalary}\t${item.holidayDaysCount}\t${item.additionalBonus}\t${item.novedades || ''}\t${item.totalToCollect}`;
+      return `${item.branchName}\t${item.employeeName}\t${item.positionLabel}\t${item.salaryType === 'hourly' ? 'POR HORA' : 'MENSUAL'}\t${item.salaryType === 'hourly' ? item.hoursWorked : '-'}\t${item.baseRateOrSalary}\t${item.holidayDaysCount}\t${item.additionalBonus}\t${item.novedades || ''}\t${(item.paymentMethod || 'transferencia').toUpperCase()}\t${item.totalToCollect}`;
     });
     
     const content = [headers, ...rows].join('\n');
